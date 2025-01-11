d3223640b33f46ffec261139b25f312ef0686f9d
Hassaanjbaig-code <hassaanb000@gmail.com>
2025-01-12 00:45:03 +0500

    CLinic is created

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,20 +1,29 @@
 class ClinicsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_doctor_register, only: %i[new create]
+
   def new
     @clinic = @doctor.clinics.new
   end
 
   def create
-    
+    @doctor_register = DoctorRegister.find_by_id(params[:doctor_register_id])
+    respond_to do |format|
+      if @doctor_register.update(clinics_params)
+        format.html { redirect_to root_path, notice: "Clinic is Created" }
+        format.json { render :new, status: :created, location: @doctor }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
+
   def set_doctor_register
     @doctor = current_user.doctor_register
   end
-  def clinics_params
-    param.require(:doctor_register).permit(clinics_attributes: [:clinic_name, :address, :contact_number])
 
+  def clinics_params
+    params.require(:doctor_register).permit(clinics_attributes: [:clinic_name, :address, :contact_number])
   end
 end
--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,5 +1,9 @@
 class Clinic < ApplicationRecord
   belongs_to :doctor_register
   geocoded_by :address
+
+  validates :clinic_name, presence: true
+  validates :address, presence: true
+  validates :contact_number, presence: true
   # after_validation validation_method, on: :create
 end
