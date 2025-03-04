6773a89bc2afaafd3ef8b132db0f8b530cf0bb61
Hassaanjbaig-code <hassaanb000@gmail.com>
2025-03-04 13:02:01 +0500

    fix the url of the elastic search

--- app/workers/expire_date_schedular.rb
+++ app/workers/expire_date_schedular.rb
@@ -8,6 +8,7 @@ class ExpireDateSchedular
     p "ExpireDate is Working"
 
     date = StartEndMedicine.where("end_time < ?", Date.today)
+    puts "This is the result of the start and end date", date
     if date.empty?
       p "Date is not empty"
     else
